f3b06249a0d8e77c9a63a9d28c90013639dd49c6
LOL4to <36468727+LOL4to@users.noreply.github.com>
2021-11-24 20:22:27 +0300

    create: added newsletter and adaptive

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ var mySwiper = new Swiper(".swiper", {
 	},
 	effect: "coverflow",
 });
+var myMap;
+ymaps.ready(function () {
+    myMap = new ymaps.Map("YMapsID", {
+        center: [37.777339, -122.414567],
+        zoom: 13
+     });
+		myMap.setType('yandex#map');
+		myMap.hint.show([],
+    '<em>ГЛО академия</em> лучшая?');
+});
